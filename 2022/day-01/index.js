9f220a4ecc0de1ee9efa74239ab78f073e783312
eko-develops <ekodevelops@gmail.com>
2022-12-02 16:25:48 -0800

    finish day 1

--- 2022/day-01/index.js
+++ 2022/day-01/index.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+const { getData } = require('../../scripts.js');
+
+const parseData = (data) => {
+	return data
+		.toString()
+		.split(/\r?\n/)
+		.map((calories) => parseInt(calories));
+};
+
+const main = () => {
+	const data = getData('../../data/elf-calories.txt', 'utf8');
+	const result = parseData(data);
+
+	const elves = [];
+
+	let total = 0;
+
+	for (let calorie of result) {
+		if (!isNaN(calorie)) {
+			total += calorie;
+		} else {
+			elves.push(total);
+			total = 0;
+		}
+	}
+
+	const highestCalorieElf = elves.sort((a, b) => a - b).at(-1);
+
+	console.log(
+		`The elf holding the most calories has ${highestCalorieElf} calories.`
+	);
+};
+
+main();
