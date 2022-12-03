00c5bd03f047fdbfa70c7d4c1eb5d882a81a6ef3
eko-develops <ekodevelops@gmail.com>
2022-12-02 16:31:49 -0800

    add rest

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const readFile = () => {
-	const data = fs.readFileSync('2022/day-01/input.txt', 'utf8');
-	const result = data.split('\n');
-	return result;
+const getData = (customPath, options) => {
+	return fs.readFileSync(path.resolve(`${customPath}`), options);
 };
 
-console.log(readFile());
+module.exports = {
+	getData,
+};
