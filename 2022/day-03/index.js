df22d846826a54ca3eab90d21fd9825d2bd07f03
eko-develops <ekodevelops@gmail.com>
2022-12-03 17:22:21 -0800

    finish day 3 p2

--- 2022/day-03/index.js
+++ 2022/day-03/index.js
@@ -4,22 +4,55 @@ const main = () => {
 	const data = getData('rucksacks.txt', 'utf8');
 	const rucksacks = parseData(data);
 
-	const commonItems = [];
-	for (const rucksack of rucksacks) {
-		const middle = rucksack.length / 2;
-		const firstRucksack = rucksack.slice(0, middle);
-		const secondRucksack = rucksack.slice(middle);
+	// part 1
+	const commonItems = getCommonItemsPriority(rucksacks);
+	const totalPriority = getTotalPriority(commonItems);
+	console.log(`The sum of total priorities is ${totalPriority}`);
 
-		for (let i = 0; i <= firstRucksack.length; i++) {
-			if (secondRucksack.includes(firstRucksack[i])) {
-				commonItems.push(convertCharCode(firstRucksack[i]));
-				break;
+	//part2
+	// [ [1, 2, 3], [4, 5, 6],  ...]
+	const groups = getGroups(rucksacks);
+
+	const groupBadgesCodes = getGroupBadgeCodes(groups);
+	const groupPriorityTotals = getTotalPriority(groupBadgesCodes);
+	console.log(
+		`The sum of total priorites for badges is ${groupPriorityTotals}`
+	);
+};
+
+const getGroupBadgeCodes = (groups) => {
+	const badges = [];
+
+	for (let i = 0; i < groups.length; i++) {
+		const currentGroup = groups[i];
+		for (let j = 0; j < currentGroup.length; j++) {
+			const first = currentGroup[j];
+
+			for (let k = 0; k < first.length; k++) {
+				const currentChar = first[k];
+				if (
+					currentGroup[j + 1].includes(currentChar) &&
+					currentGroup[j + 2].includes(currentChar)
+				) {
+					badges.push(convertCharCode(currentChar));
+					break;
+				}
 			}
+			break;
 		}
 	}
 
-	const totalPriority = getTotalPriority(commonItems);
-	console.log(`The sum of total priorities is ${totalPriority}`);
+	return badges;
+};
+
+const getGroups = (rucksacks) => {
+	const groups = [];
+	const groupSize = 3;
+	for (let i = 0; i < rucksacks.length; i += groupSize) {
+		const group = rucksacks.slice(i, i + groupSize);
+		groups.push(group);
+	}
+	return groups;
 };
 
 const parseData = (data) => {
@@ -34,6 +67,25 @@ const convertCharCode = (char) => {
 	}
 
 	return char.charCodeAt(0) - difference;
+};
+
+const getCommonItemsPriority = (rucksacks) => {
+	const commonItems = [];
+
+	for (const rucksack of rucksacks) {
+		const middle = rucksack.length / 2;
+		const firstRucksack = rucksack.slice(0, middle);
+		const secondRucksack = rucksack.slice(middle);
+
+		for (let i = 0; i <= firstRucksack.length; i++) {
+			if (secondRucksack.includes(firstRucksack[i])) {
+				commonItems.push(convertCharCode(firstRucksack[i]));
+				break;
+			}
+		}
+	}
+
+	return commonItems;
 };
 
 const getTotalPriority = (commonItems) => {
