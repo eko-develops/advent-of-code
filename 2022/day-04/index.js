2f7a9eac3edd24ec3fc180ec37c68c91208e1e83
eko-develops <ekodevelops@gmail.com>
2022-12-04 18:19:25 -0800

    finish day 4 pt 2

--- 2022/day-04/index.js
+++ 2022/day-04/index.js
@@ -8,9 +8,40 @@ const main = () => {
 
 	//pt 1
 	const amountContained = getTotalAmountContained(sectionPairs);
-	console.log(`The total amount of sections overlapping is ${amountContained}`);
+	console.log(`The total amount of sections contained is ${amountContained}`);
 
 	//pt 2
+	const amountOverlapping = getTotalOverlapping(sectionPairs);
+	console.log(
+		`The total amount of sections overlapping is ${amountOverlapping}`
+	);
+};
+
+const getTotalOverlapping = (sectionPairs) => {
+	let totalOverlapping = 0;
+	sectionPairs.forEach(([first, second]) => {
+		const [firstStarting, firstEnding] = first.split('-');
+		const [secondStarting, secondEnding] = second.split('-');
+
+		const firstPair = [];
+		for (let i = parseInt(firstStarting); i <= parseInt(firstEnding); i++) {
+			firstPair.push(parseInt(i));
+		}
+
+		const secondPair = [];
+		for (let i = parseInt(secondStarting); i <= parseInt(secondEnding); i++) {
+			secondPair.push(parseInt(i));
+		}
+
+		for (const number of firstPair) {
+			if (secondPair.includes(number)) {
+				totalOverlapping++;
+				break;
+			}
+		}
+	});
+
+	return totalOverlapping;
 };
 
 const getTotalAmountContained = (sectionPairs) => {
