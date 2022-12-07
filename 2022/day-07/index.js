e8bcdbe06d80a171235b9aa83cbf067b8430e972
eko-develops <ekodevelops@gmail.com>
2022-12-07 00:40:31 -0800

    work on traversing the directories:

--- 2022/day-07/index.js
+++ 2022/day-07/index.js
@@ -2,7 +2,30 @@ const { getData } = require('../../scripts.js');
 
 const main = () => {
 	const data = getData('commands.txt', 'utf8');
-	const commands = data.toString().split(/\r?\n/).slice(0, 3);
+	// const commands = data.toString().split(/\r?\n/).slice(0, 100);
+	const commands = data.toString().split(/\r?\n/);
+
+	// console.log(commands);
+	getTotalDirSum(commands);
+};
+
+const getTotalDirSum = (commands) => {
+	let total = 0;
+
+	commands.forEach((command) => {
+		const number = command.match(/\d/g);
+		console.log(number);
+		if (number !== null) {
+			const parsedNumber = parseInt(number.join(''));
+			const limit = 100000;
+			if (parsedNumber <= limit) {
+				total += parsedNumber;
+			}
+		}
+	});
+	console.log(total);
+
+	// console.log(result);
 };
 
 /**
